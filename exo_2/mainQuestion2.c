069ca483fd3882172b412b987e51c334fc526dfb
Erwan <cuir-saunas.07@icloud.com>
2023-12-08 16:08:59 +0100

    **bugfix: Fixing problem of reversing data.**
    _Commit description:_
    
    - Fixing problem of reversing data (MPA_PRIVATE -> MAP_SHARED.
    - Correcting the munmap to delete the right place.
    - Adding some verifications on munmap and fsync.
    
    [CR #002]

--- exo_2/mainQuestion2.c
+++ exo_2/mainQuestion2.c
@@ -27,7 +27,7 @@ void mainQuestion2()
     printf("Size of file: %ld bytes.\n\n", statsOfFile.st_size);
 
     // 3. Mapping the file into memory
-    char* mappedData = mmap(NULL, statsOfFile.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, openFile, 0);
+    char* mappedData = mmap(NULL, statsOfFile.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, openFile, 0);
     printf("The file has been mapped in memory.\n");
 
     // 4. Inverse bytes of the file in memory.
@@ -40,7 +40,16 @@ void mainQuestion2()
     printf("Bytes of the file have been reversed.\n");
 
     // 5. End the mapping and propagate the info.
-    munmap(NULL, statsOfFile.st_size);
-    fsync(openFile);
+    int unmappedData = munmap(mappedData, statsOfFile.st_size);
+    if (-1 == unmappedData)
+    {
+        perror("ERROR on munmap");
+    }
+    int fsyncData = fsync(openFile);
+    if (-1 == fsyncData)
+    {
+        perror("ERROR on fsync");
+    }
+
     printf("The file has been unmapped and synchronization forced.\n");
 }
